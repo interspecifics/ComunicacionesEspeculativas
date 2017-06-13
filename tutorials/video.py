36c420a90c41e39024b7e760988948c4ccf66256
Microhom <microhom@gmail.com>
2017-06-13 18:08:26 -0300

    cv2 importar video

---
+++ tutorials/video.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+#Crear code y crear objeto VideoWriter
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
+
+while (cap.isOpened()):
+    ret,frame = cap.read()
+    if ret==True:
+        frame = cv2.flip(frame, 0)
+
+        #escribir la imagen invertida
+        out.write(frame)
+
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        else:
+            break
+
+
+# cerrar si el trabajo se ha terminado
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
